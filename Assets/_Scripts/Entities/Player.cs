46cfc2bf9e12952e1ab7b7b1c526b0968e9c418d
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-07-17 21:20:25 +0700

    Add gun rotation

--- Assets/_Scripts/Entities/Player.cs
+++ Assets/_Scripts/Entities/Player.cs
@@ -1,48 +1,58 @@
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField][Range(1000, 10000)] private float speed;
+    [SerializeField] private Transform hand;
+    [SerializeField] private GameObject gunPrefab;
+
+    private Transform gunTf;
+    private Vector2 moveDirection;
+
+
     private Rigidbody2D rb;
     private Animator anim;
     private Transform tf;
-
-    private Vector2 direction;
-    [SerializeField][Range(1000, 10000)] private float speed;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         tf = GetComponent<Transform>();
+        gunTf = Instantiate(gunPrefab, hand).GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        direction = new();
-        if (Input.GetKey(KeyCode.W)) direction.y += 1;
-        if (Input.GetKey(KeyCode.A)) direction.x -= 1;
-        if (Input.GetKey(KeyCode.S)) direction.y -= 1;
-        if (Input.GetKey(KeyCode.D)) direction.x += 1;
-        anim.SetBool("Running", direction.magnitude > 0);
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        moveDirection = new();
+        if (Input.GetKey(KeyCode.W)) moveDirection.y += 1;
+        if (Input.GetKey(KeyCode.A)) moveDirection.x -= 1;
+        if (Input.GetKey(KeyCode.S)) moveDirection.y -= 1;
+        if (Input.GetKey(KeyCode.D)) moveDirection.x += 1;
+        anim.SetBool("Running", moveDirection.magnitude > 0);
 
-        if (tf.position.x < mousePosition.x)
-        {
-            tf.localScale = Vector3.Lerp(tf.localScale, new Vector3(1, 1, 1), 0.1f);
-        }
-        else
-        {
-            tf.localScale = Vector3.Lerp(tf.localScale, new Vector3(-1, 1, 1), 0.1f);
-        }
+        // Flips the player if mouse is on the left side of the player
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition = new Vector3(mousePosition.x, mousePosition.y, 0);
+        float targetScale;
+        if (tf.position.x < mousePosition.x) targetScale = 1;
+        else targetScale = -1;
+        tf.localScale = Vector3.Lerp(tf.localScale, new Vector3(targetScale, 1, 1), 0.1f);
+
+        // Make gun look at cursor
+        gunTf.rotation = Quaternion.LookRotation(Vector3.forward, (mousePosition - gunTf.position).normalized);
+        gunTf.Rotate(Vector3.forward, 90.0f);
+        if (tf.position.x > mousePosition.x) gunTf.Rotate(Vector3.forward, 180.0f);
     }
 
     void FixedUpdate()
     {
-        if (direction.magnitude > 0)
+        if (moveDirection.magnitude > 0)
         {
-            rb.AddForce(speed * Time.deltaTime * direction.normalized);
+            rb.AddForce(speed * Time.deltaTime * moveDirection.normalized);
         }
     }
 }
